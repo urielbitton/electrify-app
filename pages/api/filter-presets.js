928a290306de5fecc2911903f7aef7c4e9460e9c
Atomics <info@atomicsdigital.com>
2022-01-01 18:37:30 -0500

    redeploy test 4

--- pages/api/filter-presets.js
+++ pages/api/filter-presets.js
@@ -14,6 +14,6 @@ export const presets = [
   {name: 'xpro2'},
 ]
 
-export default function handler(req, res) {
-  res.status(200).json(presets)
-}
+// export default function handler(req, res) {
+//   res.status(200).json(presets)
+// }
